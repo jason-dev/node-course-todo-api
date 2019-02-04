895560ee5eea9ecfa7c0469165fb03f7a499684b
Jason May <jmay1978@gmail.com>
2019-02-03 22:31:13 -0600

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ var {
   User
 } = require('./models/user');
 
+var {
+  authenticate
+} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT;
 
@@ -119,6 +123,10 @@ app.post('/users', (req, res) => {
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => res.status(400).send(e));
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
